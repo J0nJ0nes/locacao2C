d543ac9bc135c2970d90994d36cfa3d4f585e425
jonatan <extremeshow046@gmail.com>
2025-05-22 07:33:40 -0300

    Delete backend directory

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-//chamada das dependencias
-const express = require('express');
-const http =require('http');
-
-//Inicializar Aplicação Express
-const app =express();
-const server =http.createServer(app);
-
-//conexões com BD e Escutar Servidor

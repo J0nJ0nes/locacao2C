0235e6ea08ed0148c29749be4bc7003197a13ca2
jonatan <extremeshow046@gmail.com>
2025-05-22 08:01:51 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-//chamada das dependencias
+//Chamada das dependencias
 const express = require('express');
-const http =require('http');
+const http = require('http');
 
-//Inicializar Aplicação Express
-const app =express();
-const server =http.createServer(app);
+//inicializar aplicação Express
+const app = express();
+const server = http.createServer(app);
 
-//conexões com BD e Escutar Servidor
+//Inicializar conexões
+const inicializar = async () => {
+    try {
+        //Escutar o servidor na porta 3000
+        const PORT = process.env.PORT;
+        server.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+        });
+    } catch {
+        console.log('Erro ao inicializar o servidor:',error);
+        process.exit(1);
+    }
+};
+
+//executando a função inicializar()
+inicializar();
+
+module.exports = { app, server};
